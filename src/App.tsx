0401dec155a99c0fc601e82e4996b45eccd51fe6
Teona Piranishvili <teona.piranishvili1@gmail.com>
2022-12-26 19:00:55 +0400

    style: add search style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [listItems, setListItems] = useState<any>([]);
+  useEffect(() => {
+    const getItemsList = async () => {
+      const res = await axios.get(
+        "https://entertainment-web.onrender.com/api/film"
+      );
+      setListItems(res.data);
+    };
+    getItemsList();
+  }, []);
   return (
     <div className="md:p-5">
       <Routes>
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home listItems={listItems} />} />
       </Routes>
     </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./Header";
+import search from "../images/icon-search.svg";
 
-const Home = () => {
-  const [listItems, setListItems] = useState<any>([]);
-  useEffect(() => {
-    const getItemsList = async () => {
-      const res = await axios.get(
-        "https://entertainment-web.onrender.com/api/film"
-      );
-      setListItems(res.data);
-    };
-    getItemsList();
-  }, []);
-  console.log(listItems);
+const Home = ({ listItems }: any) => {
   return (
-    <div>
+    <div className="xl:flex">
       <Header />
+      <div className="flex px-2 md:px-0 py-7 xl:h-full xl:w-full xl:p-[4%]">
+        <img src={search} />
+        <input
+          className="bg-black w-full ml-3 xl:text-2xl"
+          placeholder="Search for movies or TV series"
+        ></input>
+      </div>
       {/* {listItems.map((item: any) => (
         <div className="flex justify-between mt-2">
           <p>{item.title}</p>
           <img
             src={`http://entertainment-web.onrender.com/images/${item.regularSmall}`}
           ></img>
-           <img src={`http://localhost:5000/images/${item.regularSmall}`}></img>
-          <img src={`http://localhost:5000/images/${item.regularLarge}`}></img> 
           <div className="flex  "></div>
         </div>
       ))} */}
